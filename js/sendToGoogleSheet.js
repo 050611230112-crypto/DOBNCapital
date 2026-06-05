f4972fb502b1396249e150fde61dbf4fd981a8ad
DOBN <050611230112@st.buh.edu.vn>
2026-06-05 22:42:44 +0700

    Update sendToGoogleSheet.js

--- js/sendToGoogleSheet.js
+++ js/sendToGoogleSheet.js
@@ -1,38 +1,21 @@
 async function sendToGoogleSheet(formData){
-
     const scriptURL =
     "https://script.google.com/macros/s/AKfycbxgMDHkltVnKlDWQ6HGK8B3ZPRVRo1lNP8Em5Jnd5-qdbHFLw4NT-9KXub7qch_eg06Fg/exec";
-
     try{
-
-        const response = await fetch(scriptURL, {
-
+        await fetch(scriptURL, {
             method: "POST",
-
+            mode: "no-cors",  // ← thêm
             headers: {
                 "Content-Type": "application/json"
             },
-
             body: JSON.stringify(formData)
-
         });
-
-        const result = await response.json();
-
-        console.log(
-            "Đã lưu khách hàng:",
-            result
-        );
-
+        console.log("Đã gửi dữ liệu lên Google Sheet");
     }
-
     catch(error){
-
         console.error(
             "Lỗi Google Sheet:",
             error
         );
-
     }
-
 }
